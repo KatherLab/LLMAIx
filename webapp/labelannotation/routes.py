ba0e4efb04f8b02ac9731b36103af9ee20e1c2a1
Fabian Wolf <fabian@fawolf.de>
2024-06-13 14:56:19 +0200

    Labelannotation: correctly delete id column from annotation labels

--- webapp/labelannotation/routes.py
+++ webapp/labelannotation/routes.py
@@ -250,7 +250,7 @@ def generate_report_dict(row, df_annotation) -> dict:
     # if len(df_annotation[df_annotation["id"].str.startswith(row.report_id_short)]) > 1:
     #     raise Exception("Multiple annotations found for report " + row.report_id_short)
 
-    del report_dict["annotation_labels"]["report"]
+    del report_dict["annotation_labels"]["id"]
     # similar with llm output labels from the row, excluding id, report, metadata, matching_report, no_matching_report, report_redacted
     report_dict["llm_output_labels"] = {
         k: v
