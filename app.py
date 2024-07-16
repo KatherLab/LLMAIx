c801e2de5a90c2d16c6e4a92156d829fec0c68b3
Fabian Wolf <fabian@fawolf.de>
2024-07-16 16:44:47 +0200

    Fix hostname not passed & add password basic auth

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ if __name__ == "__main__":
 
     print("Start Server on http://" + args.host + ":" + str(args.port))
 
-    socketio.run(app, debug=args.debug, use_reloader=args.debug, port=args.port)
+    socketio.run(app, debug=args.debug, use_reloader=args.debug, port=args.port, host=args.host)
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, Response, request
 from flask_socketio import SocketIO
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 socketio = SocketIO()
@@ -8,6 +10,28 @@ def create_app():
     app = Flask(__name__)
 
     app.secret_key = 'jf894puwt8ahg9piofdmhv78943oewmhrtfsud98pmhor3e8r9pi'
+
+    auth = HTTPBasicAuth()
+
+    # Dictionary to store users and their hashed passwords (in a real application, use a database)
+    users = {
+        #"katherlab": generate_password_hash("kinetic-thesaurus-repurpose"),
+        "katherlab": "scrypt:32768:8:1$GERvnWuqIlJqAiFS$e57df57d7d6b31c05ddc9771d71a8685fcb25ebe8fd6d8253ae5b14052a764f78a5a92b007d5dc90507535383c634f7fed58fe8389759b3c7d3cbfcccf6b3e93"
+    }
+
+    print(users['katherlab'])
+
+    @auth.verify_password
+    def verify_password(username, password):
+        if username in users:
+            return check_password_hash(users.get(username), password)
+        return False
+
+    # Apply authentication to all routes
+    @app.before_request
+    def before_request():
+        # Call the login_required method
+        return auth.login_required()(lambda: None)()
 
     from .input_processing import input_processing
     from .llm_processing import llm_processing
@@ -21,7 +45,11 @@ def create_app():
     app.register_blueprint(main)
 
     socketio.init_app(app)
+
     return app
+    
+
+    
 
 def set_mode(session, mode):
     pass
